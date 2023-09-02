376d78901d4001189bcb7569abd70dc7e636e369
yogesh <142290331+yogeshbura25@users.noreply.github.com>
2023-09-02 13:34:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,10 @@ import TeamMatches from './components/TeamMatches'
 import './App.css'
 
 const App = () => (
-  // FIX2: Routes should be wrapped with Switch component from react-router-dom
+
   <Switch>
-    {/* FIX3: "exact" keyword should be added */}
-    <Route exact path="/" component={Home} />
-    {/* FIX4: The Route component should be given the prop "component" */}
-    {/* FIX5:  When mentioning path parameters for a route we need to use ":" before the variable */}
-    <Route path="/team-matches/:id" component={TeamMatches} />
+     <Route exact path="/" component={Home} />
+     <Route path="/team-matches/:id" component={TeamMatches} />
     <Route component={NotFound} />
   </Switch>
 )
